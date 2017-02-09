053ede49873e403cab6333a41e9e603aa745bcdb
agustin1554 <ag-1998@hotmail.com>
2017-02-09 10:03:22 -0300

    ejercicio 10
    de instruccion if, una de las formas correctas de resolverlo

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,24 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	var nota = Math.floor((Math.random() * 100) + 1);
+	alert(nota);
+
+	if(nota>8)
+	{
+		alert("excelente");
+	}
+
+	if(nota<9 && nota>3)
+	{
+		alert("aprobo");
+	}
+
+	if(nota<4)
+	{
+		alert("vamos, para la proxima se puede");
+	}
+
+
+
+}//FIN DE LA FUNCIÓN
